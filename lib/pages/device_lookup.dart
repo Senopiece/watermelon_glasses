241f0ce3141ff9d2bf6d5e3033e694c7e7d87355
Eugeniy <zensorok29@gmail.com>
2022-06-18 03:30:21 +0300

    Manage pages

--- lib/pages/device_lookup.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'package:watermelon_glasses/views/device_lookup_root.dart';
-
-final deviceLookupPage = GetPage(
-  name: '/device_lookup',
-  page: () => const DeviceLookupRoot(),
-  binding: BindingsBuilder(() {}),
-);
