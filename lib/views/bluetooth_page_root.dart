03fd4264979c8ad70b8747fc921e18a7be8d6aba
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-06-19 22:52:42 +0300

    feat: add list of available devices

--- lib/views/bluetooth_page_root.dart
+++ lib/views/bluetooth_page_root.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
+import '../datatypes/device.dart';
+import '../datatypes/bluetooth.dart';
+
 class BluetoothPageRoot extends StatelessWidget {
-  const BluetoothPageRoot({Key? key}) : super(key: key);
+  BluetoothPageRoot({Key? key}) : super(key: key);
+  final _bluetoothDevices = [
+    Device(name: 'device1', id: '0', bluetooth: Bluetooth()),
+    Device(name: 'device2', id: '1', bluetooth: Bluetooth()),
+    Device(name: 'device3', id: '2', bluetooth: Bluetooth()),
+    Device(name: 'device4', id: '3', bluetooth: Bluetooth()),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("3"),),
+      body: ListView.separated(
+        itemCount: _bluetoothDevices.length,
+        itemBuilder: (context, index) {
+          return Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.only(top: 10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Device Name: ${_bluetoothDevices[index].name}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Text(
+                    'Bluetooth Name: ${_bluetoothDevices[index].bluetooth.name}'),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (context, int index) => Divider(
+          height: 10,
+        ),
+      ),
     );
   }
 }
