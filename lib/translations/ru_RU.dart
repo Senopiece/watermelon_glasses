79c57153e7b6da76576353456a446e6dfeadd6e1
Senopiece <shichiromail@gmail.com>
2022-06-30 12:18:32 +0300

    paraphrase translation

--- lib/translations/ru_RU.dart
+++ lib/translations/ru_RU.dart
@@ -1,13 +1,12 @@
 const Map<String, String> ruRU = {
-  "Language" : "Язык",
-  "Language label" : "Русский",
-  "Common" : "Стандартные",
-  "Dark theme" : "Тёмная тема",
-  "no device connected" : "устройства не найдены",
-  "Channel" : "Канал",
-  "Bluetooth" : "Подключение",
-  "Time" : "Выбор времени",
-  "Manual" : "Ручное управление",
-  "Settings" : "Настройки",
-
+  "Language": "Язык",
+  "Language label": "Русский",
+  "Common": "Стандартные",
+  "Dark theme": "Тёмная тема",
+  "no device connected": "нет подключенного устройства",
+  "Channel": "Канал",
+  "Bluetooth": "Подключение",
+  "Time": "Выбор времени",
+  "Manual": "Ручное управление",
+  "Settings": "Настройки",
 };
